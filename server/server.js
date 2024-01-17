4b5fd28458a798582e934c29c0dbb7f58c96e72d
UttkarshSharma23 <uttkarsh.sharma2019@gmail.com>
2024-01-17 11:56:54 +0530

    perfect file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+// dotenv acts as middlesware which allows this file to be used throughout this Project.
 /*
 // if someone visits the homePage first time so default root page and the function
 app.get("/",(req,res)=>{
@@ -15,18 +17,32 @@ app.get("/register",(req,res)=>{
 const express = require("express")
 const app = express();
 const router = require("./router/auth-router")
+const connectDb = require("./utils/db")
+
 
 // MiddleWare: Application using json, important to place before any routes
 app.use(express.json());
 // Mount the Router : To use the router in your main Express app we can mount it at specific URL prefix
 app.use("/api/auth", router)
 
+
+
 app.get("/" , (req, res)=>
 {
     res.status(200).send("welcome Uttkarsh")
 })
 
+// This lives up the server
 const PORT = 5000;
+/*
 app.listen(PORT,() =>{
     console.log(`server is running at port: ${PORT}`)
 })
+*/
+
+// Backend connected with databse.
+connectDb().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running at port: ${PORT}`)
+    })
+}) 
